171af804c9759a91b23257658ce1d0cdbdb0ef6f
samir-tahir <samirtahir91@gmail.com>
2024-03-24 20:40:02 +0000

    up

--- internal/controller/githubapp_controller.go
+++ internal/controller/githubapp_controller.go
@@ -22,12 +22,13 @@ import (
 	"fmt"
 
 	"github.com/google/go-github/v60/github"
-	"github.com/go-logr/logr"
+	"golang.org/x/oauth2"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/log"
-
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	githubappv1 "github-app-operator/api/v1"
 	corev1 "k8s.io/api/core/v1"
 	
