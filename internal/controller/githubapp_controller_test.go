e9fe26a23b3817f8ceb2d53901c5ab6ad6d23527
samir-tahir <samirtahir91@gmail.com>
2024-03-25 16:00:02 +0000

    up

--- internal/controller/githubapp_controller_test.go
+++ internal/controller/githubapp_controller_test.go
@@ -57,6 +57,7 @@ var _ = Describe("GithubApp controller", func() {
 				Data: map[string][]byte{"privateKey": []byte(privateKey)},
 			}
 			Expect(k8sClient.Create(ctx, &secret1Obj)).Should(Succeed())
+			fmt.Println("Private Key:", privateKey)
 
 			By("Creating a first GithubApp custom resource in the sourceNamespace")
 			githubApp := githubappv1.GithubApp{
