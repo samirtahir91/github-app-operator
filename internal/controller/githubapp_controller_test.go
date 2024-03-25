94325f857b5bc26db8f39a4377205d23b31bf251
samir-tahir <samirtahir91@gmail.com>
2024-03-25 22:17:46 +0000

    up

--- internal/controller/githubapp_controller_test.go
+++ internal/controller/githubapp_controller_test.go
@@ -107,7 +107,7 @@ var _ = Describe("GithubApp controller", func() {
 			// Print the result
 			fmt.Println("Reconciliation result:", result)
 			// Add a sleep to allow the controller to trigger requeue
-			time.Sleep(60 * time.Second)
+			time.Sleep(90 * time.Second)
 		})
 	})
 })
