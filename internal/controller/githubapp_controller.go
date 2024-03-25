9c98ac9e023e1d82cab68185d7ae6ab3e80459ff
samir-tahir <samirtahir91@gmail.com>
2024-03-25 15:04:34 +0000

    up

--- internal/controller/githubapp_controller.go
+++ internal/controller/githubapp_controller.go
@@ -52,6 +52,7 @@ var (
 //+kubebuilder:rbac:groups=githubapp.samir.io,resources=githubapps,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=githubapp.samir.io,resources=githubapps/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=githubapp.samir.io,resources=githubapps/finalizers,verbs=update
+//+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;update;create;delete;watch;patch
 
 // Reconcile function
 func (r *GithubAppReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
