655cd1270c16c5296e7a08a26017520beebe8655
samir-tahir <samirtahir91@gmail.com>
2024-03-26 00:21:32 +0000

    up

--- internal/controller/githubapp_controller.go
+++ internal/controller/githubapp_controller.go
@@ -37,6 +37,7 @@ import (
 
 	"sigs.k8s.io/controller-runtime/pkg/predicate" // Required for Watching
 	"sigs.k8s.io/controller-runtime/pkg/builder"   // Required for Watching
+	"sigs.k8s.io/controller-runtime/pkg/event"     // Required for Watching
 
 )
 
