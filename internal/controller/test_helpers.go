39f1479f6f60ee5b5ef42b52fa07320341677b95
samir-tahir <samirtahir91@gmail.com>
2024-04-02 09:59:14 +0100

    up tets

--- internal/controller/test_helpers.go
+++ internal/controller/test_helpers.go
@@ -9,6 +9,9 @@ import (
 	"os"
 	"strconv"
 
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
 	githubappv1 "github-app-operator/api/v1"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
