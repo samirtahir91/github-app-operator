4db846439cd4ddc474171f38d2da40a85f13d27c
samir-tahir <samirtahir91@gmail.com>
2024-04-03 10:00:48 +0100

    up

--- internal/controller/githubapp_controller.go
+++ internal/controller/githubapp_controller.go
@@ -564,7 +564,7 @@ func (r *GithubAppReconciler) rolloutDeployment(ctx context.Context, githubApp *
         for _, deployment := range deploymentList.Items {
 
 			// Add a timestamp label to trigger a rolling upgrade
-			deployment.ObjectMeta.Labels["lastUpdateTime"] = time.Now().Format("20060102150405")
+			deployment.ObjectMeta.Annotations["lastUpdateTime"] = time.Now().Format(time.RFC3339)
 			
 			// Patch the Deployment
 			if err := r.Update(ctx, &deployment); err != nil {
